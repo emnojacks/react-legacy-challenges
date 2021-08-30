bfa98e7994aa729fc4da2722e3fdebdc330d97cb
Emily Jackson <emj@Emilys-Air.lan>
2021-08-29 21:54:22 -0400

    atomic

--- src/Components/Search/SearchIndex.js
+++ src/Components/Search/SearchIndex.js
@@ -32,21 +32,17 @@ class SearchIndex extends Component {
     })
     }
 
-      
+    console.log(this.state.resultsArr);
     // //copy the array in state
     // console.log(this.state.search);
     // //empty array
     // console.log(this.state.resultsArr);
     // //full array 
     // console.log(this.state.things);
-    
-    console.log(this.state.resultsArr);
     //console.log(resultsArr)
     //console.log(typeof this.state.resultsArr);
     //search the existing array for the query and create a new one with the results
     //resultsArr.filter(supply => supply == this.state.search);
-    
-    
     //console.log(this.state.resultsArr)
     //if there is a match (if the filtered array has a length of 1, then set the state of the suppliesArr to the resultsArr, if not, then do nothing to suppliesArr
     // if (resultsArr.length == 1) {
@@ -59,10 +55,7 @@ class SearchIndex extends Component {
   
   
 
-  render() {
-    // let { suppliesArr } = suppliesArr;
-    // console.log(suppliesArr);
-    
+  render() {   
       return (
         <div>
           <p>Search the school supplies list for an item.  If it exists on the list, it will return.  If not, the entire list will be displayed. 
@@ -86,7 +79,6 @@ class SearchIndex extends Component {
           <h3>Results:</h3>
         
             {this.state.resultsArr.map((supply, index)=><li key={index}>{supply}</li>)}
-         
       
         </div>
       );
